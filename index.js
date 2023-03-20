fcefe5e7a5498d3bbeabd2a7fc791a0086b18109
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-03-20 21:56:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName.toUpperCase();
+};
+
+
+
+var bestCustomer;
+function setBestCustomer() {
+   bestCustomer='not bob'   
+}
+
+
+function overwriteBestCustomer() {
+ bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = " Mary";
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer= "Penny";
+ //console.log(changeLeastFavoriteCustomer(leastFavoriteCustomer));
+ }
